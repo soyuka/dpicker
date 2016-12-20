e71d826a6e4f654a2059b15ff4180868ed7221f1
abluchet <abluchet@ds-restauration.com>
2016-12-20 16:49:47 +0100

    chore(): Run test before bump

--- bump.sh
+++ bump.sh
@@ -3,6 +3,7 @@
 [[ '' == $1 ]] && echo "Please provide patch, minor, major argument" && exit 1
 
 gulp
+npm test
 newver=$(npm --no-git-tag-version version $1)
 git add -f dist package.json
 git commit -m $newver
