6cf76f13a0de817fcdf1533a5085c8c2cf368404
abluchet <abluchet@ds-restauration.com>
2017-03-31 16:26:08 +0200

    add gz size to build

--- build.sh
+++ build.sh
@@ -9,18 +9,20 @@ build() {
         args="-s DPicker $args"
   fi
 
-  sh -c "$browserify $args src/$1 -o dist/$1"
+  sh -c "$browserify $args src/$1 -o dist/$1.js"
   sh -c "$browserify -g uglifyify $args src/$1 -o dist/$1.min.js"
 }
 
 rm dist/* &> /dev/null
 mkdir dist &> /dev/null
-build "dpicker.js" 1 &
-build "dpicker.time.js" &
-build "dpicker.modifiers.js" &
-build "dpicker.arrow-navigation.js" &
+build "dpicker" 1 &
+build "dpicker.time" &
+build "dpicker.modifiers" &
+build "dpicker.arrow-navigation" &
 $browserify -g uglifyify src/polyfills.js -o dist/polyfills.min.js &
 
 wait
+
+echo $(node -pe "$(gzip -c dist/dpicker.min.js | wc -c) * 0.001") kb gz
 
 exit 0
