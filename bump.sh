0f284c7a4b48246e35662e2a2df7d8827851380e
soyuka <soyuka@gmail.com>
2017-04-04 13:54:55 +0200

    Add prerelease ability to publish script

--- bump.sh
+++ bump.sh
@@ -2,13 +2,19 @@
 
 [[ '' == $1 ]] && echo "Please provide patch, minor, major argument" && exit 1
 
+tag='next'
+
+if [[ $1 == 'patch' || $1 == 'minor' || $1 == 'major' ]]; then
+        tag='latest'
+fi
+
 gulp
 npm test
 newver=$(npm --no-git-tag-version version $1)
 git add -f dist package.json
 git commit -m $newver
 git tag $newver
-npm publish
+npm publish --tag $tag
 git reset --hard HEAD~1
 newver=$(npm --no-git-tag-version version $1)
 git add package.json
