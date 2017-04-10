43f44e469c40d9198c8e49a806cab8329e7e6598
abluchet <abluchet@ds-restauration.com>
2017-04-10 18:16:27 +0200

    release docs script

--- .scripts/release-docs.sh
+++ .scripts/release-docs.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 git checkout gh-pages
 git reset --hard origin/master
-bower install
-gulp build-styles
-npm run coverage
-./node_modules/.bin/jsdoc -c jsdoc.conf.json -R README.md -d . src/*.js
-git add .
-git add -f coverage
-git add -f dist
-git add -f demo/index.html
-git add -f demo/styles.html
+bash .scripts/build-docs.sh
+mv docs/* .
+git add -f _api.md
+git add -f demo/public/dist
+git add -f .
 git commit -m 'bump doc'
 git push -fu origin gh-pages
 git checkout master
