da68d204d638cae974d98ceb2431ad516a76549e
Philip <philip@whitespace.gmbh>
2017-08-04 08:33:10 +0200

    require it for dpicker.all

--- src/all.js
+++ src/all.js
@@ -3,5 +3,6 @@ require('./plugins/time.js')(DPicker)
 require('./plugins/modifiers.js')(DPicker)
 require('./plugins/arrow-navigation.js')(DPicker)
 require('./plugins/navigation.js')(DPicker)
+require('./plugins/monthAndYear.js')(DPicker)
 
 module.exports = DPicker
